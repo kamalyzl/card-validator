c6d6e1f2454ac17b441bc453ea75a757ac126994
kamalyzl <kamaly.kazal@gmail.com>
2018-01-30 20:11:24 -0500

    agregando funcion de cvv, exp y data

--- src/app.js
+++ src/app.js
@@ -1,37 +1,78 @@
+const cl = console.log;
+let number = document.getElementById('cn'),
+  btn = document.getElementById('btn'),
+  name = document.getElementById('name'),
+  exp = document.getElementById('exp'),
+  cvv = document.getElementById('cvv');
 
-let number = document.getElementById('cn');
-let btn = document.getElementById('btn');
-let name = document.getElementById('name');
-let exp = document.getElementById('exp');
-let cvv = document.getElementById('cvv');
-
-// Evento final, indicamos si los datos son correctos
-btn.addEventListener('click', () => {
-  let value = number.value;
-  validCard(value) ? alert('valido!') : alert('invalido!'); // validCard es la función
-});
-
+let verifyNumber = false, verifyName = false, verifyCvv = false, verifyExp = false;
+// validCard es la función
+let validNumber = () => {
+  let val = number.value;
+  if (validCard(val) && val.length === 16) {
+    btnActive();
+    verifyNumber = true;
+    return true;
+  } else {
+    btnDesactive();
+    return false;
+  }
+};
 // Evento que valida si existe solo texto
-name.addEventListener('keyup', () => {
+let validName = () => {
   let name = document.getElementById('name');
   let val = name.value;
   let regex = /^[a-zA-Z\s]*$/;
-  if (regex.test(val)) {
+  if (regex.test(val) && val.length > 3) {
+    verifyName = true;
+    btnActive();
     return true;
   } else {
+    btnDesactive();
     return false;
   }
-});
+};
 
 // Evento que valida si ingresa 3 numeros, caso contrario desabilita el campo 
-cvv.addEventListener('keyup', () => {
+let validCvv = () => {
   let cvv = document.getElementById('cvv');
   let value = cvv.value;
   let regex = /^\d{3}/;
   if (regex.test(value)) {
+    verifyCvv = true;
     cvv.setAttribute('disabled', true);
+    btnActive();
   } else {
-    // ...
+    btnDesactive();
   }
-});
+};
 
+// Evento que valida la fecha de expiracion
+let validExp = () => {
+  let exp = document.getElementById('exp');
+  let val = exp.value;
+  if (val) {
+    verifyExp = true;    
+    btnActive();
+  } else {
+    btnDesactive();
+  }
+};
+
+btnActive = () => {
+  if (verifyNumber && verifyName && verifyCvv && verifyExp) {
+    btn.disabled = false;
+  }
+};
+btnDesactive = () => {
+  btn.disabled = true;
+};
+
+button = () => {
+  alert('valido!');
+};
+cvv.addEventListener('keyup', validCvv);
+name.addEventListener('keyup', validName);
+number.addEventListener('keyup', validNumber);
+exp.addEventListener('change', validExp);
+btn.addEventListener('click', button);
---
+++ src/data.js
@@ -0,0 +1,18 @@
+class Persona {
+  constructor(number, name, cvv, fechExp) {
+    this.number = number;
+    this.name = name;
+    this.cvv = cvv;
+    this.fechExp = fechExp;
+  }
+}
+
+const joalbert = new Persona(4578565808288696, 'Joalbert', 123, '2018-01');
+const mariana = new Persona(4694998705775813, 'Mariana', 456, '2018-02');
+const alejandra = new Persona(4260808615995621, 'Alejandra', 789, '2018-03');
+// console.log(joalbert.number);
+// console.log(joalbert.name);
+// console.log(joalbert.cvv);
+// console.log(joalbert.fechExp);
+// console.log(mariana);
+// console.log(alejandra);
