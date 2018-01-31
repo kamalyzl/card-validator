917b75f15948325c0d9c5918893d60c5544a419c
kamalyzl <kamaly.kazal@gmail.com>
2018-01-31 14:19:42 -0500

    Acutalizando carpetas

--- src/data.js
+++ src/data.js
@@ -19,25 +19,3 @@ let persona = [
 
   }
 ];
-
-document.getElementById('send').addEventListener('click', function() {
-  // Trayendo valores de los inputs
-  const cardNumber = document.getElementById('card-number').value;
-  const cvv = parseInt(document.getElementById('cvv').value);
-  const name = document.getElementById('name').value;
-
-  // Obteniendo los datos del cliente en base a su número de tarjeta
-  const client = persona.filter((val) => val.number == cardNumber)[0];
-
-  // Solo pasará a validar los siguientes datos si se encuentra el número de tarjeta ingresado en la data
-  if (client) {
-    // Validando si los demás inputs son correctos
-    if (client.name == name && client.cvv == cvv) {
-      console.log(client.name);
-      console.log(client.cvv);
-      console.log('este usuario es correcto');
-    }
-  } else {
-    console.log('El usuario no es válido');
-  }
-});
