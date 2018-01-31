d8810d1e395fd6cdc932d329bca9dbf46d270e43
JackelineGCP <gessenia_17@hotmail.com>
2018-01-31 01:26:28 -0500

    subiendo data.js

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 
-// Clase que contiene datos creados para generar pruebas 
+// Clases que contienes datos creados para generar pruebas 
 class Persona {
   constructor(number, name, cvv, fechExp) {
     this.number = number;
@@ -12,3 +12,43 @@ class Persona {
 const joalbert = new Persona(4578565808288696, 'Joalbert', 123, '2018-01');
 const mariana = new Persona(4694998705775813, 'Mariana', 456, '2018-02');
 const alejandra = new Persona(4260808615995621, 'Alejandra', 789, '2018-03');
+/*
+var dataPeron = {
+  'dataperson': [
+    {
+      'number': 4884358916188092,
+      'name': 'Jorge Maguina',
+      'exp': '08-2019',
+      'cvv': 562
+      
+    },
+    {
+      'number': 4329511051291559,
+      'name': 'Rosario Cajavilca',
+      'exp': '08-2018',
+      'cvv': 568
+      
+    },
+    {
+      'number': 4523324596330583,
+      'name': 'Elisa Juarez',
+      'exp': '02-2020',
+      'cvv': 985
+     
+    },
+    {
+      'number': 4523324596330583,
+      'name': 'César Munayco',
+      'exp': '05-2022',
+      'cvv': 562
+      
+    },
+    {
+      'number': 4802639110455992,
+      'name': 'Percy Soto',
+      'exp': '01-2021',
+      'cvv': 253
+    }
+  ]
+};
+*/
