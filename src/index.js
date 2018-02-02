59b3dd10257704ddee543116ecf0871b6db6c651
kamalyzl <kamaly.kazal@gmail.com>
2018-02-01 20:42:28 -0500

    funciones de validacion completadas

--- src/index.js
+++ src/index.js
@@ -1,38 +1,57 @@
-// Utilizamos el patron IIFE, Expresión de función inmediatamente invocada, función anonima auto ejecutable
-// Así prevenimos el Hoisting
-
+const cl = console.log;
 (function() {
-  // Objeto Card contiene función ValidCard
-  let card = {
-    validCard: number => {
-      let sum = 0,
-        alt = false,
-        i = number.length - 1, // recorriendo de manera inversa
-        num;
-      if (number.length !== 16) {
-        return false;
-      }
-      while (i >= 0) {
-        num = parseInt(number.charAt(i), 10); // obteniendo el segundo digito    
-        if (isNaN(num)) {
-          return false;
-        }
-        if (alt) {
-          num *= 2;
-          if (num > 9) {
-            num = (num % 10) + 1;
+  card = {
+    // Funcion que valida la longitud del input ingresado por el usuario
+    validNumber(input) {
+      let regex = /^\d*$/;
+      if (!input.search(regex))
+        return input;
+      else return false;
+    },
+    validName(input) {
+      let regex = /^[a-zA-Z\s]*$/;
+      if (regex.test(cadena))
+        return cadena;
+      else return false;
+    },
+    validCvv(input) {
+      let regex = /^\d{3}$/;
+      if (regex.test(input)) return input
+      // cvv.setAttribute('disabled', true);       
+      else cl('errorCvv')
+    },
+    validMonth(input) {
+      let regex = /^\d{2}$/;
+      if (regex.test(input))
+        return input;
+      else return false;
+    },
+    validYear(input) {
+      let regex = /^\d{2}$/;
+      if (regex.test(input))
+        return input;
+      else return false;
+    },
+    validCard(numberCard) {
+      if (numberCard) {
+        let sumaTotal = 0;
+        let revserNum = [...numberCard].reverse(); // obteniendo array inverso  
+        for (let index = 1; index < revserNum.length; index = index + 2) {
+          revserNum[index] = revserNum[index] * 2;
+          if (revserNum[index] >= 10) {
+            revserNum[index] = revserNum[index] - 9;
           }
         }
-        alt = !alt;
-        sum += num;
-        i--;
-      }
-      // retora si es valida  
-      return (sum % 10 === 0);
+        for (let value of revserNum) {
+          sumaTotal = sumaTotal + parseInt(value);
+        }
+        if (sumaTotal % 10 === 0) {
+          return true;
+          console.log('Es una tarjeta valida');
+        }
+      } else return cl('la tarjet es invalida');
     }
   };
-  // Para proteger los datos realizamos lo siguiente
-  if (typeof window.card === 'undefined') {
-    window.card = window.card = card;
-  } else console.log('hiciste uso de la libreria');
 })();
+
+
